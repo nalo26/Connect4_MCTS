66fa8b6cfebf0518fc4b601a9bd3a3efe91845a7
nalo26 <nath.v26@gmail.com>
2023-06-08 19:32:08 +0200

    Format, typing, structure

--- main.py
+++ main.py
@@ -1,54 +1,64 @@
+class Player:
+    def __init__(self, player_id):
+        self.player_id = player_id
+
+    def play(self) -> int:
+        column = int(input(f"[Player {self.player_id}] > "))
+        return column - 1
+
+
 class Game:
-    """Connect 4 game class"""
-
     def __init__(self):
-        self.board = Board()
-        self.players = [Player(i + 1) for i in range(2)]
-        self.current_player_index = 0
+        from ai import AI
 
-    def play(self):
+        self.board = Board()
+        self.players = [Player(1), AI(2)]
+        self.current_player_id = 0
+
+    def play(self) -> None:
         self.board.print_board()
         while True:
             self.play_turn()
             self.board.print_board()
             if self.check_win():
-                print(f"Player {self.current_player_index + 1} won !")
+                print(f"Player {self.current_player_id + 1} won !")
                 break
             self.switch_player()
 
-    def play_turn(self):
-        column = int(input("Which column do you want to play ?\n> ")) - 1
-        self.board.play(column, self.current_player_index)
+    def play_turn(self) -> None:
+        column = self.current_player().play()
+        self.board.play(column, self.current_player_id)
 
-    def check_win(self):
+    def current_player(self) -> Player:
+        return self.players[self.current_player_id]
+
+    def check_win(self) -> bool:
         return self.board.check_win()
 
-    def switch_player(self):
-        self.current_player_index = (self.current_player_index + 1) % 2
+    def switch_player(self) -> None:
+        self.current_player_id = (self.current_player_id + 1) % 2
 
 
 class Board:
-    """Board class"""
-
     tokens = {0: "🔘", 1: "🟡", 2: "🔴"}
 
     def __init__(self):
-        self.board = [[0 for i in range(7)] for j in range(6)]
+        self.board = [[0 for _ in range(7)] for _ in range(6)]
 
-    def print_board(self):
+    def print_board(self) -> None:
         print(" " + " ".join([str(i + 1) for i in range(7)]))
         print(" v" * 7)
         for row in self.board:
             print("".join([Board.tokens[char] for char in row]))
 
-    def play(self, column, player_index):
+    def play(self, column, player_id) -> None:
         for i in range(6):
             if self.board[i][column] != 0:
                 i -= 1
                 break
-        self.board[i][column] = player_index + 1
+        self.board[i][column] = player_id + 1
 
-    def check_win(self):
+    def check_win(self) -> bool:
         """Check if the current player has won the game"""
         # Horizontal check
         for row in self.board:
@@ -87,13 +97,6 @@ class Board:
                 ):
                     return True
         return False
-
-
-class Player:
-    """Player class"""
-
-    def __init__(self, player_number):
-        self.player_number = player_number
 
 
 if __name__ == "__main__":
