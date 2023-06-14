f297804b407d3a484d5c6171de34ac1bb82d18ad
Émilie Vey <nath.v26@gmail.com>
2023-06-14 09:22:51 +0200

    Abstract algorithms

--- ai.py
+++ ai.py
@@ -1,17 +1,18 @@
 import re
-from copy import deepcopy
 
+from algoritms import Minimax
 from main import Board, Player
-from utils import rotate_board, rows_to_strings, diagonals_board
+from utils import diagonals_board, rotate_board, rows_to_strings
 
 
 class AI(Player):
     def __init__(self, player_id, board: Board):
         super().__init__(player_id)
         self.board = board
+        self.algorithm = Minimax(self.evaluate, self.player_id)
 
     def play(self) -> int:
-        column = self.minimax(self.board, 4)
+        column = self.algorithm.run(board=self.board, max_depth=4)
         print(f"[{self}] > {column + 1}")
         return column
 
@@ -48,37 +49,3 @@ class AI(Player):
         return self.check(board, self.player_id) - self.check(
             board, self.player_id % 2 + 1
         )
-
-    def minimax(self, root: Board, max_depth: int):
-        eval, action = self.max_player(root, max_depth)
-        return action
-
-    def max_player(self, board: Board, p: int) -> tuple[int, int]:
-        if board.check_win() or p == 0:
-            return self.evaluate(board), None
-        u = -float("inf")
-        action = None
-        for a in range(7):
-            new_board = deepcopy(board)
-            if not new_board.play(a, self.player_id):
-                continue
-            eval, _ = self.min_player(new_board, p - 1)
-            if eval > u:
-                u = eval
-                action = a
-        return u, action
-
-    def min_player(self, n: Board, p: int) -> tuple[int, int]:
-        if n.check_win() or p == 0:
-            return self.evaluate(n), None
-        u = float("inf")
-        action = None
-        for a in range(7):
-            new_board = deepcopy(n)
-            if not new_board.play(a, self.player_id % 2 + 1):
-                continue
-            eval, _ = self.max_player(new_board, p - 1)
-            if eval < u:
-                u = eval
-                action = a
-        return u, action
---
+++ algoritms.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+from copy import deepcopy
+from typing import Callable
+
+from main import Board
+
+
+class Algorithm(ABC):
+    def __init__(self, evaluate_method: Callable[[Board], int], player_id: int):
+        self.evaluate = evaluate_method
+        self.player_id = player_id
+
+    @abstractmethod
+    def run(self, **kwargs):
+        pass
+
+
+class Minimax(Algorithm):
+    def run(self, **kwargs):
+        return self.minimax(kwargs["board"], kwargs["max_depth"])
+
+    def minimax(self, root: Board, max_depth: int):
+        eval, action = self.max_player(root, max_depth)
+        return action
+
+    def max_player(self, board: Board, p: int) -> tuple[int, int]:
+        if board.check_win() or p == 0:
+            return self.evaluate(board), None
+        u = -float("inf")
+        action = None
+        for a in range(7):
+            new_board = deepcopy(board)
+            if not new_board.play(a, self.player_id):
+                continue
+            eval, _ = self.min_player(new_board, p - 1)
+            if eval > u:
+                u = eval
+                action = a
+        return u, action
+
+    def min_player(self, n: Board, p: int) -> tuple[int, int]:
+        if n.check_win() or p == 0:
+            return self.evaluate(n), None
+        u = float("inf")
+        action = None
+        for a in range(7):
+            new_board = deepcopy(n)
+            if not new_board.play(a, self.player_id % 2 + 1):
+                continue
+            eval, _ = self.max_player(new_board, p - 1)
+            if eval < u:
+                u = eval
+                action = a
+        return u, action
